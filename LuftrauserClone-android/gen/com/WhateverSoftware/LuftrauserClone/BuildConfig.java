ce50f07da77e117b63570011f291bfa15aec1d7c
rakorth <pcloadletter4@gmail.com>
2014-01-22 21:08:18 -0800

    Added Art Assets
    Added some sample assets to use from
    http://opengameart.org/content/war-on-water-gfx

---
+++ LuftrauserClone-android/gen/com/WhateverSoftware/LuftrauserClone/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.WhateverSoftware.LuftrauserClone;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
