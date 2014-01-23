4f5e03c3e3f2875f148c7136e1d1f06a4626933a
aplicable <prlebold@wpi.edu>
2014-01-22 21:42:29 -0500

    Abstract Objects

---
+++ LuftrauserClone/src/com/WhateverSoftware/LuftrauserClone/Objects/AShip.java
@@ -0,0 +1,25 @@
+package com.WhateverSoftware.LuftrauserClone.Objects;
+
+import java.awt.Point;
+
+public abstract class AShip implements IEntity {
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Point getLocation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
