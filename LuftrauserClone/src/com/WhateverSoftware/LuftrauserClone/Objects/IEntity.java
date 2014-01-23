e40051d9dbdfb182c1c68009e041657baf2e5083
aplicable <prlebold@wpi.edu>
2014-01-22 21:10:10 -0500

    Addition: IEntity Class

---
+++ LuftrauserClone/src/com/WhateverSoftware/LuftrauserClone/Objects/IEntity.java
@@ -0,0 +1,9 @@
+package com.WhateverSoftware.LuftrauserClone.Objects;
+
+import java.awt.Point;
+
+public interface IEntity {
+	public void draw();
+	public void move();
+	public Point getLocation();
+}
