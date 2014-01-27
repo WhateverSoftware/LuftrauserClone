b41fd9639d10da27efd776041ff4693d671b32f9
marthaurion <marthaurion@gmail.com>
2014-01-26 22:01:21 -0600

    Removed GameTickHandler from constructor and created a setGameTickHandler. Right now, this function stores the GameTickHandler and then calls the register function to add the entity to the list.
    Also, cleaned up code from my last commit a bit.

--- LuftrauserClone/src/com/WhateverSoftware/LuftrauserClone/Objects/UserAirplane.java
+++ LuftrauserClone/src/com/WhateverSoftware/LuftrauserClone/Objects/UserAirplane.java
@@ -1,14 +1,12 @@
 package com.WhateverSoftware.LuftrauserClone.Objects;
 
-import com.WhateverSoftware.LuftrauserClone.StateManagers.IGameTickHandlerEntityView;
-
 public class UserAirplane extends AAirplane {
 
 	private static final int COOLDOWN_PERIOD = 5; //ticks
 	private static final int MAX_HEALTH = 100;
 	
-	public UserAirplane(int x, int y, int directionFacing, IGameTickHandlerEntityView gth) {
-		super(x, y, directionFacing, COOLDOWN_PERIOD, MAX_HEALTH, gth);
+	public UserAirplane(int x, int y, int directionFacing) {
+		super(x, y, directionFacing, COOLDOWN_PERIOD, MAX_HEALTH);
 		// TODO Auto-generated constructor stub
 	}
 
