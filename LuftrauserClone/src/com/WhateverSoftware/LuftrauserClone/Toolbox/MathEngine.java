bf28566f6995303e864deaec740a502ae6bad997
aplicable <prlebold@wpi.edu>
2014-01-27 20:54:17 -0500

    Updated calcVelocity method to handle max-speeds

--- LuftrauserClone/src/com/WhateverSoftware/LuftrauserClone/Toolbox/MathEngine.java
+++ LuftrauserClone/src/com/WhateverSoftware/LuftrauserClone/Toolbox/MathEngine.java
@@ -5,7 +5,7 @@ import java.awt.geom.Point2D;
 public class MathEngine {
 	//I'm just guessing with these. They can be adjusted once we get a visual representation working.
 	private static final double friction = 0.2;
-	private static final double gravity = 3;
+	private static final double gravity = 0.1;
 
 	public static double degreesToRadians(int degree){
 		return degree * Math.PI / 180;
@@ -31,7 +31,7 @@ public class MathEngine {
 	 * @param direction - angle in degrees of the plane's orientation
 	 * @return Store the x-component and y-component of new velocity in a Point2D.Double object
 	 */
-	public static Point2D.Double calcVelocity(double velx, double vely, double thrust, int direction) {
+	public static double calcVelocity(double velx, double vely, double thrust, int direction) {
 		double radDirectionFacing = MathEngine.degreesToRadians(direction);
 		double thrustX = thrust * Math.cos(radDirectionFacing);
 		double thrustY = thrust * Math.sin(radDirectionFacing);
@@ -43,6 +43,15 @@ public class MathEngine {
 		//adjust with new acceleration and gravity
 		velx += thrustX;
 		vely += thrustY - gravity;
+		
+		//scale to max_magnitude
+		int maxMagnitude = AAirplane.MAX_VELOCITY;
+		int currentMagnitude = pythagorean(velx,vely);
+		if(currentMagnitude>maxMagnitude){
+			velx = velx / currentMagnitude * maxMagnitude;
+			vely = vely / currentMagnitude * maxMagnitude;
+		}
+		
 		return new Point2D.Double(velx, vely);
 	}
 }
