84be330c91e1b56334716e8241a824600a1a6a4e
rakorth <pcloadletter4@gmail.com>
2014-02-01 18:32:35 -0800

    InputHandler and UserAirplane
    Reworked input handler to work with lib
    Added a user airplane to gamescreen for test

--- LuftrauserClone/src/com/WhateverSoftware/LuftrauserClone/Screens/GameScreen.java
+++ LuftrauserClone/src/com/WhateverSoftware/LuftrauserClone/Screens/GameScreen.java
@@ -1,6 +1,8 @@
 package com.WhateverSoftware.LuftrauserClone.Screens;
 
+import com.WhateverSoftware.LuftrauserClone.Objects.Airplanes.UserAirplane;
 import com.WhateverSoftware.LuftrauserClone.StateManagers.GameTickHandler;
+import com.WhateverSoftware.LuftrauserClone.StateManagers.KeyInputHandler;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
@@ -23,6 +25,11 @@ public class GameScreen implements Screen {
 		camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		camera.position.set(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2, 0);
 		camera.update();
+		
+		UserAirplane uA = new UserAirplane(100,100,0);
+		gameTickHandler.register(uA);
+		KeyInputHandler kIA = new KeyInputHandler(uA, true);
+		Gdx.input.setInputProcessor(kIA);
 	}
 
 	@Override
--- LuftrauserClone/src/com/WhateverSoftware/LuftrauserClone/StateManagers/KeyInputHandler.java
+++ LuftrauserClone/src/com/WhateverSoftware/LuftrauserClone/StateManagers/KeyInputHandler.java
@@ -1,57 +1,103 @@
 package com.WhateverSoftware.LuftrauserClone.StateManagers;
 
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-
 import com.WhateverSoftware.LuftrauserClone.Objects.Airplanes.UserAirplane;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 
-public class KeyInputHandler implements KeyListener {	
+public class KeyInputHandler implements InputProcessor {
 
 	private UserAirplane airplane;
 	private boolean acceptingData;
 
-	public KeyInputHandler(UserAirplane airplane, boolean acceptingData){
-		this.airplane=airplane;
-		this.acceptingData=acceptingData;
+	public KeyInputHandler(UserAirplane airplane, boolean acceptingData) {
+		this.airplane = airplane;
+		this.acceptingData = acceptingData;
 	}
 
-	public void setAcceptingData(boolean acceptingData){
-		this.acceptingData=acceptingData;
+	public void setAcceptingData(boolean acceptingData) {
+		this.acceptingData = acceptingData;
 	}
 
 	@Override
-	public void keyTyped(KeyEvent e) {}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		if(acceptingData){
-			if(e.getKeyCode() == KeyEvent.VK_X) {
-				this.airplane.setShooting(true);
-			}
-			else if(e.getKeyCode() == KeyEvent.VK_UP) {
+	public boolean keyDown(int keyCode) {
+		if (acceptingData) {
+			switch (keyCode) {
+			case Input.Keys.UP:
 				this.airplane.setThrust(true);
-			}
-			else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+				break;
+			case Input.Keys.LEFT:
 				this.airplane.setTurning(-1);
-			}
-			else if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+				break;
+			case Input.Keys.RIGHT:
 				this.airplane.setTurning(1);
+				break;
+			case Input.Keys.X:
+				this.airplane.setShooting(true);
+				break;
+			default:
+				Gdx.app.log("KEYCODE", keyCode + "");
+				break;
 			}
 		}
+		return false;
 	}
 
 	@Override
-	public void keyReleased(KeyEvent e) {
-		if(acceptingData){
-			if(e.getKeyCode() == KeyEvent.VK_X) {
-				this.airplane.setShooting(false);
-			}
-			else if(e.getKeyCode() == KeyEvent.VK_UP) {
+	public boolean keyUp(int keyCode) {
+		if (acceptingData) {
+			switch (keyCode) {
+			case Input.Keys.UP:
 				this.airplane.setThrust(false);
-			}
-			else if(e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_RIGHT) {
+				break;
+			case Input.Keys.LEFT:
+			case Input.Keys.RIGHT:
 				this.airplane.setTurning(0);
+				break;
+			case Input.Keys.X:
+				this.airplane.setShooting(false);
+				break;
+			default:
+				Gdx.app.log("KEYCODE", keyCode + "");
+				break;
 			}
 		}
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
