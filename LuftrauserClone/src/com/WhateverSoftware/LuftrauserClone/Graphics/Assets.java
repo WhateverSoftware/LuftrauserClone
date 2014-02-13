4e4f6d1d010e29c90580bce0e52e0525d1914647
aplicable <prlebold@wpi.edu>
2014-02-12 23:02:58 -0500

    Method Abstraction
    Divided images into categories

--- LuftrauserClone/src/com/WhateverSoftware/LuftrauserClone/Graphics/Assets.java
+++ LuftrauserClone/src/com/WhateverSoftware/LuftrauserClone/Graphics/Assets.java
@@ -8,8 +8,19 @@ public class Assets {
 	public static AssetManager assetManager;
 
 	public static void loadArtAssets() {
-		Assets.assetManager.load("Planes/plane1.png", Texture.class);
+		Assets.loadSkies();
+		Assets.loadClouds();
+		Assets.loadSkyLines();
+		Assets.loadBoats();
+		Assets.loadPlanes();
+		Assets.loadProjectiles();
 
+		Assets.assetManager.load("MainMenu/PlayGame.png", Texture.class);
+		Assets.assetManager.load("MainMenu/Options.png", Texture.class);
+		Assets.assetManager.load("MainMenu/ExitGame.png", Texture.class);
+	}
+	
+	public static void loadSkies(){
 		Assets.assetManager.load("Background/Backgrounds/sky01.png", Texture.class);
 		Assets.assetManager.load("Background/Backgrounds/sky02.png", Texture.class);
 		Assets.assetManager.load("Background/Backgrounds/sky03.png", Texture.class);
@@ -20,7 +31,9 @@ public class Assets {
 		Assets.assetManager.load("Background/Backgrounds/sky08.png", Texture.class);
 		Assets.assetManager.load("Background/Backgrounds/sky09.png", Texture.class);
 		Assets.assetManager.load("Background/Backgrounds/sky10.png", Texture.class);
-
+	}
+	
+	public static void loadClouds(){
 		Assets.assetManager.load("Background/Clouds/cloud1.png", Texture.class);
 		Assets.assetManager.load("Background/Clouds/cloud2.png", Texture.class);
 		Assets.assetManager.load("Background/Clouds/cloud3.png", Texture.class);
@@ -28,7 +41,9 @@ public class Assets {
 		Assets.assetManager.load("Background/Clouds/cloud5.png", Texture.class);
 		Assets.assetManager.load("Background/Clouds/cloud6.png", Texture.class);
 		Assets.assetManager.load("Background/Clouds/cloud7.png", Texture.class);
-
+	}
+	
+	public static void loadSkyLines(){
 		Assets.assetManager.load("Background/Skylines/city1.png", Texture.class);
 		Assets.assetManager.load("Background/Skylines/city2.png", Texture.class);
 		Assets.assetManager.load("Background/Skylines/city3.png", Texture.class);
@@ -39,7 +54,9 @@ public class Assets {
 		Assets.assetManager.load("Background/Skylines/mountainshills.png", Texture.class);
 		Assets.assetManager.load("Background/Skylines/mountainsisland.png", Texture.class);
 		Assets.assetManager.load("Background/Skylines/pyramids.png", Texture.class);
-
+	}
+	
+	public static void loadBoats(){
 		Assets.assetManager.load("Boats/pirateboat1.png", Texture.class);
 		Assets.assetManager.load("Boats/pirateboat2.png", Texture.class);
 		Assets.assetManager.load("Boats/pirateboat3.png", Texture.class);
@@ -55,7 +72,9 @@ public class Assets {
 		Assets.assetManager.load("Boats/ship8.png", Texture.class);
 		Assets.assetManager.load("Boats/ship9.png", Texture.class);
 		Assets.assetManager.load("Boats/ship10.png", Texture.class);
-
+	}
+	
+	public static void loadPlanes(){
 		Assets.assetManager.load("Planes/plane1.png", Texture.class);
 		Assets.assetManager.load("Planes/plane2.png", Texture.class);
 		Assets.assetManager.load("Planes/plane3.png", Texture.class);
@@ -73,15 +92,13 @@ public class Assets {
 		Assets.assetManager.load("Planes/plane15.png", Texture.class);
 		Assets.assetManager.load("Planes/tail1.png", Texture.class);
 		Assets.assetManager.load("Planes/tail2.png", Texture.class);
-
+	}
+	
+	public static void loadProjectiles(){
 		Assets.assetManager.load("Projectiles/bomb1.png", Texture.class);
 		Assets.assetManager.load("Projectiles/bomb2.png", Texture.class);
 		Assets.assetManager.load("Projectiles/bomb3.png", Texture.class);
 		Assets.assetManager.load("Projectiles/bomb4.png", Texture.class);
-
-		Assets.assetManager.load("MainMenu/PlayGame.png", Texture.class);
-		Assets.assetManager.load("MainMenu/Options.png", Texture.class);
-		Assets.assetManager.load("MainMenu/ExitGame.png", Texture.class);
 	}
 
 	public static void loadSoundAssets() {
